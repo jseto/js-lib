bbc2159a92d3592518ffc867fcd0f3eb74685fe4
jseto <jseto@jasvisio.com>
2014-09-27 12:30:43 +0200

    adding test framework requirements

---
+++ karma.conf.js
@@ -0,0 +1,33 @@
+module.exports = function(config){
+  config.set({
+
+    basePath : './',
+
+    files : [
+      'test/bower_components/angular/angular.js',
+      'test/bower_components/angular-route/angular-route.js',
+      'test/bower_components/angular-mocks/angular-mocks.js',
+      'test/components/**/*.js',
+      'test/view*/**/*.js'
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome'],
+
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-firefox-launcher',
+            'karma-jasmine',
+            'karma-junit-reporter'
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+
+  });
+};
