b804fa0d479faa7d9a814649fd92d588c2323e4a
jseto <jseto@jasvisio.com>
2015-01-16 23:52:53 +0100

    validate-promise and unique-user validator

---
+++ test/js-widgets/unique-user.spec.js
@@ -0,0 +1,74 @@
+'use strict';
+
+describe('jswUniqueUser directive', function() {
+	var element, scope, $compile, httpBackend, $http, $q;
+
+	var compile = function( scope, html ) {
+		// Compile a piece of HTML containing the directive
+		element = $compile( html )(scope);
+		// fire all the watches, so the scope expression {{1 + 1}} will be evaluated
+		scope.$digest();
+		return element;
+	};
+
+	beforeEach( module('jsWidgets.uniqueUser') );
+	
+	// Store references to $rootScope and $compile
+	// so they are available to all tests in this describe block
+	beforeEach(inject(function(_$compile_, _$rootScope_, _$httpBackend_, _$http_, _$q_){
+		// The injector unwraps the underscores (_) from around the parameter names when matching
+		$compile = _$compile_;
+		scope = _$rootScope_;
+		httpBackend = _$httpBackend_;
+		$http = _$http_;
+		$q = _$q_;
+
+		httpBackend
+			.when('GET', '/api/user/joe')
+			.respond({username: 'joe', otherUserFields: true });
+
+		httpBackend
+			.when('GET', '/api/user/bar')
+			.respond(402);
+	}));
+
+	beforeEach( function(){
+		var elmHtml = '<input jsw-unique-user="isUserUniquePromise" ng-model="model">';
+		scope.isUserUniquePromise = function( value ) {
+			return $http.get('/api/user/' + value );
+		};
+
+		element = compile(scope, elmHtml);
+	});
+
+	afterEach(function() {
+		httpBackend.verifyNoOutstandingExpectation();
+		httpBackend.verifyNoOutstandingRequest();
+	});
+
+	it('puts invalid state because user exists', function() {
+
+		scope.$apply( function() {
+			scope.model = 'joe';
+		});
+
+		httpBackend.flush();
+
+		expect(	element.hasClass('ng-valid') ).toBe(false);
+		expect(	element.hasClass('ng-invalid') ).toBe(true);
+	});
+
+	it('puts valid state because user does NOT exists', function() {
+
+		scope.$apply( function() {
+			scope.model = 'bar';
+		});
+
+		httpBackend.flush();
+
+		expect(	element.hasClass('ng-valid') ).toBe(true);
+		expect(	element.hasClass('ng-invalid') ).toBe(false);
+	});
+
+
+});
