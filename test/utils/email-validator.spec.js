72a3979dfae34c0e0b1679a0dc44af170b181404
jseto <jseto@jasvisio.com>
2015-02-01 12:20:56 +0100

    emailvalidator directive

--- test/utils/email-validator.spec.js
+++ test/utils/email-validator.spec.js
@@ -1,54 +1,55 @@
 'use strict';
 
-describe('emailValidator', function() {
+describe('emailValidator factory', function() {
 
-	var emailValidatorFilter;
+	var emailValidator;
 
 	beforeEach(module('jsLib.utils.emailValidator'));
-	beforeEach(inject(function($filter) {
-		emailValidatorFilter = $filter('emailValidator');
+
+	beforeEach(inject(function( _emailValidator_ ) {
+		emailValidator = _emailValidator_;
 	}));
 
 	it('should report valid email for valid emails', function() {
-		expect(	emailValidatorFilter( 'foo@bar.com' ) ).toBe(true);
-		expect(	emailValidatorFilter( 'foo@bar.c.om' ) ).toBe(true);
-		expect(	emailValidatorFilter( 'foo@bar.foooo.com' ) ).toBe(true);
-		expect(	emailValidatorFilter( 'foo.bar@bar.com' ) ).toBe(true);
-		expect(	emailValidatorFilter( 'foo@bar.co' ) ).toBe(true);
-		expect(	emailValidatorFilter( 'foo@bar.commmmmmmmmmmmmmm' ) ).toBe(true);
-		expect(	emailValidatorFilter( 'foo.fofofo@bar.com' ) ).toBe(true);
+		expect(	emailValidator( 'foo@bar.com' ) ).toBe(true);
+		expect(	emailValidator( 'foo@bar.c.om' ) ).toBe(true);
+		expect(	emailValidator( 'foo@bar.foooo.com' ) ).toBe(true);
+		expect(	emailValidator( 'foo.bar@bar.com' ) ).toBe(true);
+		expect(	emailValidator( 'foo@bar.co' ) ).toBe(true);
+		expect(	emailValidator( 'foo@bar.commmmmmmmmmmmmmm' ) ).toBe(true);
+		expect(	emailValidator( 'foo.fofofo@bar.com' ) ).toBe(true);
 	});
 
 	it('should report not valid email for emails without to level domain', function() {
 		// although it is a valid email, we do not allow
-		expect(	emailValidatorFilter( 'foo@bar' ) ).toBe(false);
+		expect(	emailValidator( 'foo@bar' ) ).toBe(false);
 	});
 
 	it('should report not valid email for emails with . simbol in invalid position', function() {
-		expect(	emailValidatorFilter( '.foo@bar.com' ) ).toBe(false);
-		expect(	emailValidatorFilter( 'fo..o@bar.com' ) ).toBe(false);
-		expect(	emailValidatorFilter( 'foo.@bar.com' ) ).toBe(false);
-		expect(	emailValidatorFilter( 'foo@.bar.com' ) ).toBe(false);
-		expect(	emailValidatorFilter( 'foo@bar..com' ) ).toBe(false);
-		expect(	emailValidatorFilter( 'foo@bar.com.' ) ).toBe(false);
+		expect(	emailValidator( '.foo@bar.com' ) ).toBe(false);
+		expect(	emailValidator( 'fo..o@bar.com' ) ).toBe(false);
+		expect(	emailValidator( 'foo.@bar.com' ) ).toBe(false);
+		expect(	emailValidator( 'foo@.bar.com' ) ).toBe(false);
+		expect(	emailValidator( 'foo@bar..com' ) ).toBe(false);
+		expect(	emailValidator( 'foo@bar.com.' ) ).toBe(false);
 	});
 
 	it('should report not valid email for emails with @ simbol in invalid position', function() {
-		expect(	emailValidatorFilter( '@foo@bar.com' ) ).toBe(false);
-		expect(	emailValidatorFilter( 'fo@o@bar.com' ) ).toBe(false);
-		expect(	emailValidatorFilter( 'foo@@bar.com' ) ).toBe(false);
-		expect(	emailValidatorFilter( 'foo@ba@r.com' ) ).toBe(false);
-		expect(	emailValidatorFilter( 'foo@bar@.com' ) ).toBe(false);
-		expect(	emailValidatorFilter( 'foo@bar.@com' ) ).toBe(false);
-		expect(	emailValidatorFilter( 'foo@bar.co@m' ) ).toBe(false);
-		expect(	emailValidatorFilter( 'foo@bar.com@' ) ).toBe(false);
-		expect(	emailValidatorFilter( 'foo.bar@' ) ).toBe(false);
-		expect(	emailValidatorFilter( '@foo.bar.com' ) ).toBe(false);
+		expect(	emailValidator( '@foo@bar.com' ) ).toBe(false);
+		expect(	emailValidator( 'fo@o@bar.com' ) ).toBe(false);
+		expect(	emailValidator( 'foo@@bar.com' ) ).toBe(false);
+		expect(	emailValidator( 'foo@ba@r.com' ) ).toBe(false);
+		expect(	emailValidator( 'foo@bar@.com' ) ).toBe(false);
+		expect(	emailValidator( 'foo@bar.@com' ) ).toBe(false);
+		expect(	emailValidator( 'foo@bar.co@m' ) ).toBe(false);
+		expect(	emailValidator( 'foo@bar.com@' ) ).toBe(false);
+		expect(	emailValidator( 'foo.bar@' ) ).toBe(false);
+		expect(	emailValidator( '@foo.bar.com' ) ).toBe(false);
 	});
 
 	it('should report invalid email for emails without @ simbol', function() {
-		expect(	emailValidatorFilter( 'foo' ) ).toBe(false);
-		expect(	emailValidatorFilter( 'foo.bar' ) ).toBe(false);
-		expect(	emailValidatorFilter( 'foo.bar.com' ) ).toBe(false);
+		expect(	emailValidator( 'foo' ) ).toBe(false);
+		expect(	emailValidator( 'foo.bar' ) ).toBe(false);
+		expect(	emailValidator( 'foo.bar.com' ) ).toBe(false);
 	});
 });
